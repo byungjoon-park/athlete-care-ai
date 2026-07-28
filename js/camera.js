432f6b9fedd1f5acae3570bdd343854433270cce
byungjoon-park <03031023@hanmail.net>
2026-07-28 12:20:49 +0900

    Delete js directory

--- js/camera.js
+++
@@ -1,9 +0,0 @@
-
-async function startAI(){
- const video=document.getElementById("video");
- const stream=await navigator.mediaDevices.getUserMedia({
-  video:{facingMode:"environment"}
- });
- video.srcObject=stream;
- initPose(video);
-}
--- js/rom.js
+++
@@ -1,5 +0,0 @@
-
-function calculateROM(p){
- document.getElementById("rom").innerText=
- "무릎 ROM 측정중 / 어깨 ROM 측정중";
-}
